ead040c3f3ac36cca94baa717b76e65eaab76ab3
ДАР ЕМАХО < >
2016-02-05 10:10:55 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// ;(function($){
+
+// 	$(function(){
+// 	$('.ba-slider').slick();
+// });
+// })(jQuery);
+
+;(function($){
+
+	$(function(){
+	var slider = $('.ba-slider');
+
+		slider.slick({
+			dots:false,
+			vertical:true,
+			verticalSwiping:true,
+			infinity:false,
+		});
+		});
+})(jQuery);
